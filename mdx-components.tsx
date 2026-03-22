c8a75bc4ec5e5bfda0fd5bf8e68b2cc9942920e0
PopcornDev1 <e.coiley@icloud.com>
2026-03-22 17:55:06 +1100

    feat: VulpineOS logo auto-injected into every h1 heading via mdx-components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -6,5 +6,20 @@ export function useMDXComponents(components?: Record<string, React.ComponentType
   return {
     ...themeComponents,
     ...components,
+    h1: (props: any) => {
+      const ThemeH1 = themeComponents.h1 as any
+      return (
+        <ThemeH1 {...props}>
+          <img
+            src="/VulpineOSLogo.png"
+            alt=""
+            width={28}
+            height={28}
+            style={{ verticalAlign: 'middle', marginRight: '10px', display: 'inline-block' }}
+          />
+          {props.children}
+        </ThemeH1>
+      )
+    },
   }
 }
